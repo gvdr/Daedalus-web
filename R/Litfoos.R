aabe7edd0a27c53733db7d29d4c9ebd37fd1b0da
Giulio Valentino Dalla Riva <gvdr@zoology.ubc.ca>
2017-04-11 21:17:01 -0700

    added per year

--- R/Litfoos.R
+++ R/Litfoos.R
@@ -3,13 +3,25 @@ library('dplyr')
 library('rvest')
  
 
-number_articles <- function(phrase_key){
+number_articles <- function(phrase_key,year = FALSE){
   library('xml2')
   library('dplyr')
   library('rvest')
   library('stringr')
   
+ if(year) {
   phrase_key %>%
+    str_replace_all(" ","+") %>%
+    str_c("%22",.,"%22") %>%
+    paste0("https://scholar.google.ca/scholar?q=",.,"&as_ylo=",year,"&as_yhi=",year) %>%
+    read_html() %>%
+    html_nodes("#gs_ab_md") %>%
+    html_text() %>%
+    str_extract("[:digit:]+,?[:digit:]+") %>%
+    str_replace(",","") %>%
+    as.numeric()
+  } else {
+    phrase_key %>%
     str_replace_all(" ","+") %>%
     str_c("%22",.,"%22") %>%
     paste0("https://scholar.google.ca/scholar?q=",.) %>%
@@ -19,4 +31,5 @@ number_articles <- function(phrase_key){
     str_extract("[:digit:]+,?[:digit:]+") %>%
     str_replace(",","") %>%
     as.numeric()
+ }
 }
